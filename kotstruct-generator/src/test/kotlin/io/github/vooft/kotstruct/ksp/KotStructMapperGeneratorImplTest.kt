6d020ea6f0dc07bd2ab4b8d4e8b66f6448e6542b
Nikolay Mikheev <el.vooft@gmail.com>
2024-01-08 14:36:26 +0400

    Add simple mapper generation

--- kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/ksp/KotStructMapperGeneratorImplTest.kt
+++ kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/ksp/KotStructMapperGeneratorImplTest.kt
@@ -1,45 +1,38 @@
 package io.github.vooft.kotstruct.ksp
 
-import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspSourcesDir
 import com.tschuchort.compiletesting.symbolProcessorProviders
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.slot
 import org.junit.jupiter.api.Test
+import kotlin.io.path.readText
 
-class KotStructMapperProcessorTest {
+class KotStructMapperGeneratorImplTest {
     private val kotlinSource = SourceFile.kotlin(
         "ExampleMapper.kt", """
                 import io.github.vooft.kotstruct.KotStructMapper
                 
-                data class FromDto(val id: String, val name: String)
+                data class FromDto(val id: String, val name: String, val extraProperty: String)
                 data class ToDto(val id: String, val name: String)
                 
                 interface ExampleMapper: KotStructMapper<FromDto, ToDto>
             """
     )
 
-    private val generatorSlot = slot<KSClassDeclaration>()
-    private val generator = mockk<KotStructMapperGenerator> {
-        every { generateFor(capture(generatorSlot)) } returns Unit
-    }
-
     @Test
-    fun `should invoke generator with discovered class`() {
-        val result = KotlinCompilation().apply {
+    fun test() {
+        val compilation = KotlinCompilation().apply {
             sources = listOf(kotlinSource)
-            symbolProcessorProviders = listOf(KotStructMapperProcessorProvider(generator))
+            symbolProcessorProviders = listOf(KotStructMapperProcessorProvider())
             inheritClassPath = true
             messageOutputStream = System.out // see diagnostics in real time
-        }.compile()
+        }
 
-        println(result)
+        val result = compilation.compile()
 
-        val captured = generatorSlot.captured
-        captured.simpleName.asString() shouldBe "ExampleMapper"
+        val file = compilation.kspSourcesDir.toPath().resolve("kotlin").resolve("${GENERATED_PREFIX}ExampleMapper.kt")
+        println(file.readText())
 
         result.exitCode shouldBe KotlinCompilation.ExitCode.OK
     }
