e22afeb451d76d93b8ba69564818771d53a4f50e
Nikolay Mikheev <el.vooft@gmail.com>
2024-01-17 18:32:41 +0400

    WIP

--- kotstruct-generator/src/main/kotlin/io/github/vooft/kotstruct/Utils.kt
+++ kotstruct-generator/src/main/kotlin/io/github/vooft/kotstruct/Utils.kt
@@ -1,28 +1,15 @@
 package io.github.vooft.kotstruct
 
-import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSType
-import com.google.devtools.ksp.symbol.KSTypeReference
 import java.security.MessageDigest
 import java.util.HexFormat
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
-
-
-private val MAPPER_CLASS_NAME = KotStructMapper::class.qualifiedName!!
-fun KSClassDeclaration.findKotStructMapperSupertype(): KSType? {
-    return superTypes.firstOrNull { it.qualifiedName == MAPPER_CLASS_NAME }?.resolve()
-}
 
 fun KClass<out KotStructMapper<*, *>>.findKotStructMapperSupertype(): KType {
     return supertypes.single { it.classifier == KotStructMapper::class }
 }
 
 val KClass<*>.packageName: String get() = qualifiedName!!.split(".").dropLast(1).joinToString(".")
-
-val KSTypeReference.qualifiedName: String get() = requireNotNull(resolve().declaration.qualifiedName) {
-    "Qualified name is not present at KSTypeReference $this"
-}.asString()
 
 private val shaMessageDigest = ThreadLocal.withInitial { MessageDigest.getInstance("SHA-1") }
 internal fun String.sha1(): String {
--- kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/TestUtils.kt
+++ kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/TestUtils.kt
@@ -1,26 +1,6 @@
 package io.github.vooft.kotstruct
 
-import com.tschuchort.compiletesting.KotlinCompilation
-import com.tschuchort.compiletesting.SourceFile
-import com.tschuchort.compiletesting.symbolProcessorProviders
 import org.junit.jupiter.api.DynamicTest
-
-fun createCompilation(src: SourceFile, vararg miscFiles: SourceFile): KotlinCompilation {
-    return KotlinCompilation().apply {
-        sources = listOf(src) + miscFiles.toList()
-        symbolProcessorProviders = listOf(KotStructMapperDslProcessorProvider())
-        inheritClassPath = true
-        messageOutputStream = System.out // see diagnostics in real time
-    }
-}
-
-fun compile(src: SourceFile, vararg miscFiles: SourceFile): KotlinCompilation.Result {
-    return createCompilation(src, *miscFiles).compile()
-}
-
-fun Map<String, SourceFile>.dynamicTests(body: (SourceFile) -> Unit) = map { (name, fromDtoCode) ->
-    DynamicTest.dynamicTest(name) { body(fromDtoCode) }
-}
 
 fun <T: Any> dynamicTests(vararg pairs: Pair<String, T>, block: (T) -> Unit): List<DynamicTest> = pairs.map { (name, value) ->
     DynamicTest.dynamicTest(name) { block(value) }
