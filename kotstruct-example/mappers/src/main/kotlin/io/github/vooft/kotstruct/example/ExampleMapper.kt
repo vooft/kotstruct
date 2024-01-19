5c6c7b4f875967b8f5a15f081ebb92ec7e036dd7
Nikolay Mikheev <el.vooft@gmail.com>
2024-01-19 14:28:35 +0000

    Remove generics, introduce mapping descriptor (#10)

--- kotstruct-example/mappers/src/main/kotlin/io/github/vooft/kotstruct/example/ExampleMapper.kt
+++ kotstruct-example/mappers/src/main/kotlin/io/github/vooft/kotstruct/example/ExampleMapper.kt
@@ -2,7 +2,10 @@ package io.github.vooft.kotstruct.example
 
 import io.github.vooft.kotstruct.KotStructMapper
 import java.util.UUID
-interface ExampleMapper: KotStructMapper<ExampleMapper.FromDto, ExampleMapper.ToDto> {
+interface ExampleMapper: KotStructMapper {
+
+    fun map(src: FromDto): ToDto
+
     data class FromDto(val id: UUID, val name: String, val extraProperty: UUID)
     data class ToDto(val name: String, val id: UUID)
 }
--- kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/dataclass/onetoone/happy/HappyPathDataClassTargetOneToOneTest.kt
+++ kotstruct-generator/src/test/kotlin/io/github/vooft/kotstruct/dataclass/onetoone/happy/HappyPathDataClassTargetOneToOneTest.kt
@@ -7,6 +7,9 @@ import io.github.vooft.kotstruct.GENERATED_PACKAGE
 import io.github.vooft.kotstruct.GENERATED_PREFIX
 import io.github.vooft.kotstruct.KotStructMapper
 import io.github.vooft.kotstruct.KotStructMapperDslProcessorProvider
+import io.github.vooft.kotstruct.dataclass.onetoone.happy.HappyPathDataClassTargetOneToOneTest.Mappers.RegularClassWithLateinitFieldsMapper
+import io.github.vooft.kotstruct.dataclass.onetoone.happy.HappyPathDataClassTargetOneToOneTest.Mappers.RegularClassWithoutBackingFieldsMapper
+import io.github.vooft.kotstruct.dataclass.onetoone.happy.HappyPathDataClassTargetOneToOneTest.Mappers.SimpleDataClassMapper
 import io.github.vooft.kotstruct.dynamicTests
 import io.kotest.matchers.paths.shouldExist
 import io.kotest.matchers.shouldBe
@@ -21,7 +24,7 @@ class HappyPathDataClassTargetOneToOneTest {
     @TestFactory
     fun `should generate when target is data class and source`() = dynamicTests(
         "is a data class" to SimpleDataClassMapper::class,
-        "is not a data class with calculated fields" to RegularClassCalculatedFieldsMapper::class,
+        "is not a data class with calculated fields" to Mappers.RegularClassCalculatedFieldsMapper::class,
         "is not a data class and without backing fields" to RegularClassWithoutBackingFieldsMapper::class,
         "is not a data class with lateinit var fields" to RegularClassWithLateinitFieldsMapper::class,
     ) {
@@ -42,25 +45,36 @@ class HappyPathDataClassTargetOneToOneTest {
         result.exitCode shouldBe KotlinCompilation.ExitCode.OK
     }
 
-    data class SimpleDataClassSourceDto(val id: String, val name: String, val extraProperty: String)
-    interface SimpleDataClassMapper : KotStructMapper<SimpleDataClassSourceDto, TestTargetDto>
+    @Suppress("unused")
+    class Mappers {
+        data class SimpleDataClassSourceDto(val id: String, val name: String, val extraProperty: String)
+        interface SimpleDataClassMapper : KotStructMapper {
+            fun map(src: SimpleDataClassSourceDto): TestTargetDto
+        }
 
-    class RegularClassCalculatedFieldsSourceDto {
-        val id = "id" + UUID.randomUUID()
-        val name = "name" + UUID.randomUUID()
+        class RegularClassCalculatedFieldsSourceDto {
+            val id = "id" + UUID.randomUUID()
+            val name = "name" + UUID.randomUUID()
+        }
+        interface RegularClassCalculatedFieldsMapper : KotStructMapper {
+            fun map(src: RegularClassCalculatedFieldsSourceDto): TestTargetDto
+        }
+
+        class RegularClassWithoutBackingFieldsSourceDto {
+            val id: String get() = "id" + UUID.randomUUID()
+            val name: String get() = "name" + UUID.randomUUID()
+        }
+        interface RegularClassWithoutBackingFieldsMapper : KotStructMapper {
+            fun map(src: RegularClassWithoutBackingFieldsSourceDto): TestTargetDto
+        }
+
+        class RegularClassWithLateinitFieldsSourceDto {
+            lateinit var id: String
+            lateinit var name: String
+        }
+
+        interface RegularClassWithLateinitFieldsMapper : KotStructMapper {
+            fun map(src: RegularClassWithLateinitFieldsSourceDto): TestTargetDto
+        }
     }
-    interface RegularClassCalculatedFieldsMapper : KotStructMapper<RegularClassCalculatedFieldsSourceDto, TestTargetDto>
-
-    class RegularClassWithoutBackingFieldsSourceDto {
-        val id: String get() = "id" + UUID.randomUUID()
-        val name: String get() = "name" + UUID.randomUUID()
-    }
-    interface RegularClassWithoutBackingFieldsMapper : KotStructMapper<RegularClassWithoutBackingFieldsSourceDto, TestTargetDto>
-
-    class RegularClassWithLateinitFieldsSourceDto {
-        lateinit var id: String
-        lateinit var name: String
-    }
-
-    interface RegularClassWithLateinitFieldsMapper : KotStructMapper<RegularClassWithLateinitFieldsSourceDto, TestTargetDto>
 }
